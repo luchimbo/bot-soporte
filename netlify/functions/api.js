eeef237cdef281e2a3f3158e6b1a876070a5c142
luchimbo <luciotambo@gmail.com>
2026-03-25 11:32:10 -0300

    cleanup: Eliminar archivos restantes de Netlify
    Eliminados:
    - netlify.toml (configuración de Netlify)
    - netlify/functions/api.js (función serverless)
    - docs/netlify-kommo-setup.md (documentación de Netlify)
    
    El proyecto ya no usa Netlify, solo servidor propio con Express.

--- netlify/functions/api.js
+++
@@ -1,7 +0,0 @@
-const serverless = require("serverless-http");
-
-const { app } = require("../../src/server");
-
-const handler = serverless(app);
-
-exports.handler = async (event, context) => handler(event, context);
